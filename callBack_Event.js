e332c96ba235ec4e1be08ff9fb7f07e50b047864
MUKUL KUMAR <mukuliter2023@gmail.com>
2026-03-10 22:55:05 +0530

    Add files via upload

---
+++ callBack_Event.js
@@ -0,0 +1,18 @@
+    // // 1. what is call back function ?-----
+
+    // setTimeout(function(){ // here we are passing an anonymous function as a call back function to setTimeout function. so this anonymous function is called a call back function because it is called back by the setTimeout function after the specified time. in this case it will print "hello world" after 1 second.
+    //     console.log("hello world");
+    // }, 5000); // here we are passing the time in milliseconds. so it will print "hello world" after 1 second.
+
+    // function x(y){
+    //     console.log("x");
+    //     y();
+    // }
+
+
+    // x(function y(){ // here we are passing a named function as a call back function to x function. so this named function is called a call back function because it is called back by the x function when we call it. so it will print "x" and then it will print "y" because we are calling the call back function inside the x function.
+    //     console.log("y");
+    // });
+
+
+    
